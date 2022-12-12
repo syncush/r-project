9c75e55170779298058a381a1d82331dd3a7e1ab
syncush <danielhermonsync@gmail.com>
2022-12-12 21:58:36 +0200

    refactor: fixed from character to date in dataframes

--- code.R
+++ code.R
@@ -1,9 +1,19 @@
-# Read the csvs
-june_csv = read.csv("Export_8_06_2022.csv")
-july_csv = read.csv("Export_8_07_2022.csv")
-august_csv = read.csv("Export_8_08_2022.csv")
+# Variables for Pavel
+checquing_account_date_format = "%d.%m.%Y"
+credit_card_date_format = "%d/%m/%Y"
 
-checquing_account_csv = read.csv("chequing_account.csv")
+june_csv_path = "Export_8_06_2022.csv"
+july_csv_path = "Export_8_07_2022.csv"
+august_csv_path = "Export_8_08_2022.csv"
+
+checquing_account_csv_path = "chequing_account.csv"
+
+# Read the csvs
+june_csv = read.csv(june_csv_path)
+july_csv = read.csv(july_csv_path)
+august_csv = read.csv(august_csv_path)
+
+checquing_account_csv = read.csv(checquing_account_csv_path)
 
 # Change the Hebrew name of the columns into English
 english_col_names_credit = c( "date", "transaction_sum", "transaction_type", "business_name", "category")
@@ -21,6 +31,14 @@ july_csv = cbind(july_csv, account="credit card")
 august_csv = cbind(august_csv, account="credit card")
 
 checquing_account_csv = cbind(checquing_account_csv, account="chequing")
+
+# prep dates in each sheet
+june_csv$date = as.Date(june_csv$date, credit_card_date_format)
+july_csv$date = as.Date(july_csv$date, credit_card_date_format)
+august_csv$date = as.Date(august_csv$date, credit_card_date_format)
+
+checquing_account_csv$date = as.Date(checquing_account_csv$date, checquing_account_date_format)
+
 
 cpi_values = c(1 , 2 , 3)
 credit_card_data = rbind(june_csv, july_csv, august_csv)
