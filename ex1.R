98dedf063e02d9ff8cd33b47b5880b457e5420cd
syncush <danielhermonsync@gmail.com>
2023-02-15 21:51:56 +0200

    feat: trying to solve x-monitor chart

--- ex1.R
+++ ex1.R
@@ -1,10 +1,10 @@
 # Install Pacakages
 #install.packages("tidyverse")
 #install.packages("ggpubr")
-
+#install.packages('patchwork')
 # Import packages
 library(tidyverse)
-library(ggpubr)
+library(patchwork)
 
 # Variables for Pavel
 
@@ -216,3 +216,116 @@ cumulativeImpliedDailyInflation <- function(monthCPI, monthsNameVec) {
   }
   tibble(date=daysVec, cdcpi_dm=cdcpi_dm)
 }
+
+# function that returns the sum of expenditures between two dates 
+sumExpendituresPerDay = function(df, startDate, endDate) { 
+  df %>%  
+    filter(date >= startDate & date <= endDate) %>% 
+    group_by(date) %>% 
+    summarise(sum=sum(debit, na.rm = TRUE)) %>% 
+    complete(date=seq.Date(min(date), max(date), by="day"), fill = list(sum=0)) 
+} 
+
+# function that returns the mean daily expenditure between two dates 
+meanExpenditurePerDay <- function(df, startDate, endDate) { 
+  df %>% 
+    filter(date >= startDate & date <= endDate & debit > 0) %>% 
+    group_by(date) %>% 
+    summarise(mean=mean(debit, na.rm = TRUE)) %>% 
+    complete(date=seq.Date(startDate, endDate, by="day"), fill = list(mean=0)) 
+} 
+
+drawExpenditureOverMonthPlot <- function(tbl, minDate=min_analysis_date, maxDate=max_analysis_date) { 
+  sumEPM <- tbl %>% 
+    group_by(date=lubridate::floor_date(date, "month")) %>% 
+    summarize(sumExpenditure = sum(debit, na.rm = TRUE)) 
+  ggplot() + 
+    geom_line(data=sumExpendituresPerDay(tbl, minDate, maxDate), aes(date, sum,), colour="blue") +  
+    geom_step(data=sumEPM, aes(date, sumExpenditure), colour="red", size=1.2) + 
+    labs(y = "Sum of Expenditures (Shekels)", x = "Time (days)") 
+} 
+
+
+drawMeanExpenditurePerDayPlot <- function(tbl, minDate=min_analysis_date, maxDate=max_analysis_date) { 
+  boxplotData <- tbl %>% 
+    filter(date >= minDate & date <= maxDate & debit > 0) 
+  ggplot() + 
+    geom_line(data=meanExpenditurePerDay(tbl, minDate, maxDate), aes(date, mean,)) + 
+    geom_boxplot(data=boxplotData, aes(date, debit, group=date)) + 
+    labs(title = "Expenditures Per Day", x = "Time (days)", y = "Mean Expenditure Per Day (Shekels)") 
+} 
+
+drawSumExpenditureHeatmapPerCategory <- function(dataet, minDate=min_analysis_date, maxDate=max_analysis_date) { 
+  dataet %>% 
+    filter(date >= minDate & date <= maxDate) %>% 
+    group_by(month=months(date), category) %>% 
+    summarise(sum=sum(debit)) %>% 
+    ggplot(aes(x=month, y=category, fill=sum, alpha)) + 
+    geom_tile() +
+    scale_fill_gradient(low="green", high="red") + 
+    labs(title = "Expenditure Heatmap By Month and Category", x = "Months", y = "Category Name") 
+} 
+
+drawMeanExpenditureOverMonth <- function(tbl, minDate=min_analysis_date, maxDate=max_analysis_date) { 
+  stepData <- tbl %>% 
+    filter(date >= minDate & date <= maxDate & debit > 0) %>% 
+    group_by(date=months(date)) %>% 
+    summarize(expenditureMean=mean(debit, na.rm = TRUE)) 
+  stepPlot <- ggplot() + 
+    geom_line(data=stepData, aes(x=factor(date, levels = month.name), group=1, y=expenditureMean)) +
+    geom_point(data=stepData, aes(date, expenditureMean)) +
+    labs(title="Daily Median Expenditures (Over a month)", x="Months", y="Expenditures (Shekels)") 
+  
+  boxplotData <- tbl %>% 
+    filter(date >= minDate & date <= maxDate & debit > 0) %>% 
+    group_by(date=months(date)) 
+  
+  boxplotPlot <- ggplot() +  
+    geom_boxplot(data=boxplotData, aes(x=factor(date, levels = month.name), y=debit, group=date)) + 
+    labs(title="Monthly Median Expenditures", x="Months", y="Expenditures (Shekels)") 
+  stepPlot + boxplotPlot
+}
+
+# Nominal Expenditures
+patch <- (drawExpenditureOverMonthPlot(combined_data) + drawSumExpenditureHeatmapPerCategory(combined_data)) /
+          (drawMeanExpenditureOverMonth(combined_data))
+patch + plot_annotation(title = 'Nominal Expenditures')
+
+# Real Expenditures 1
+combinedDataWithMonthName <- tibble(combined_data) %>% mutate(month_name=months(date))
+realExpendCombinedData <- left_join(combinedDataWithMonthName, cumulativeMontlyInflation(general_month_info), by=c("month_name")) %>% mutate(debit=debit/(1+ccpi)) %>% select(colnames(combined_data))
+patch <- (drawExpenditureOverMonthPlot(realExpendCombinedData) + drawSumExpenditureHeatmapPerCategory(realExpendCombinedData)) /
+  (drawMeanExpenditureOverMonth(realExpendCombinedData))
+patch + plot_annotation(title = 'Real Expenditures 1 (CCPI)')
+
+# Real Expenditure 2
+realExpenditureCDCPI <- left_join(tibble(combined_data), 
+                                  cumulativeImpliedDailyInflation(general_month_info, month.name[6:10]),
+                                  by=c("date")) %>%
+                        mutate(debit=debit / (1 + cdcpi_dm)) %>% 
+                        select(colnames(combined_data))
+patch <- (drawExpenditureOverMonthPlot(realExpenditureCDCPI) + drawSumExpenditureHeatmapPerCategory(realExpenditureCDCPI)) /
+  (drawMeanExpenditureOverMonth(realExpenditureCDCPI))
+patch + plot_annotation(title = 'Real Expenditures 2 (CDCPI)')
+
+
+createXMonitorChart <- function(xChartCombinedData, title) {
+  ggplot(xChartCombinedData) +
+    geom_point(aes(x=date, y=sum)) +
+    geom_line(aes(x=date, y=sum)) +
+    geom_line(aes(x=date, y=cl), color="red") +
+    geom_line(aes(x=date, y=lcl), linetype="dashed", color="red") +
+    geom_line(aes(x=date, y=ucl), linetype="dashed", color="red") +
+    labs(title=title, x="Time", y="Daily Sum Expenditure")
+}
+####### Monitoring Charts
+xChartCombinedData <- sumExpendituresPerDay(combined_data, min_analysis_date, max_analysis_date)
+# not the correct value
+firstMonth = "June"
+# Dont know if baseCL is what he meant
+baseCL <- 
+baseSD <- xChartCombinedData %>% filter(months(date) == firstMonth) %>% summarise(stdDev=sd(sum)) %>% pull() %>% nth(1)
+ucl <- baseCL + 3 * baseSD
+lcl <- max(0, baseCL - 3 * baseSD)
+xChartCombinedData <- xChartCombinedData %>% mutate(cl=baseCL, lcl=lcl, ucl=ucl)
+createXMonitorChart(xChartCombinedData, "X-Chart Daily Sum Expenditure")
